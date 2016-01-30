6485eff7a2be5f2cf6429544230e287a16822c67
andrea.como <andrea.como@gmail.com>
2016-01-30 15:35:30 +0100

    PostProcessors refactored

--- src/main/java/it/cosenonjaviste/alfresco/annotations/processors/runtime/WebScriptConfigurer.java
+++ src/main/java/it/cosenonjaviste/alfresco/annotations/processors/runtime/WebScriptConfigurer.java
@@ -1,16 +1,10 @@
 package it.cosenonjaviste.alfresco.annotations.processors.runtime;
 
 import it.cosenonjaviste.alfresco.annotations.WebScript;
-import org.apache.log4j.Logger;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.FatalBeanException;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.core.Ordered;
-import org.springframework.core.PriorityOrdered;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.util.StringUtils;
 
@@ -21,51 +15,33 @@ import org.springframework.util.StringUtils;
  * Creates webscript bean name according to Alfresco requirements (<tt>webscript.[name].[http-method]</tt>)
  * from {@link WebScript} instance
  *
- *
  * @author Andrea Como
  */
-public class WebScriptConfigurer implements BeanFactoryPostProcessor, PriorityOrdered {
+public class WebScriptConfigurer extends AbstractPostProcessorConfigurer {
 
-    private static final Logger LOGGER = Logger.getLogger(WebScriptConfigurer.class);
 
-    private int order = Ordered.LOWEST_PRECEDENCE;// default: same as non-Ordered
-
-    public void setOrder(int order) {
-        this.order = order;
-    }
-
-    public int getOrder() {
-        return this.order;
-    }
-
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+    @Override
+    protected void processBeanDefinition(ConfigurableListableBeanFactory beanFactory, BeanDefinition bd, String beanClassName, String definitionName) throws FatalBeanException {
         if (beanFactory instanceof DefaultListableBeanFactory) {
             DefaultListableBeanFactory factory = (DefaultListableBeanFactory) beanFactory;
-            String[] beanDefinitionNames = factory.getBeanDefinitionNames();
-            for (String definitionName : beanDefinitionNames) {
-                try {
-                    final BeanDefinition bd = beanFactory.getBeanDefinition(definitionName);
-                    final String beanClassName = bd.getBeanClassName();
-                    if (StringUtils.hasText(beanClassName)) {
-                        try {
-                            final WebScript webScript = AnnotationUtils.findAnnotation(Class.forName(beanClassName), WebScript.class);
-                            if (webScript != null) {
-                                final String webScriptName = "webscript." + webScript.value() + "." + webScript.method().toString().toLowerCase();
-                                factory.removeBeanDefinition(definitionName);
-                                factory.registerBeanDefinition(webScriptName, bd);
-                            }
-                        } catch (ClassNotFoundException e) {
-                            LOGGER.error(e.getMessage(), e);
-                            throw new FatalBeanException("Unknown class defined.", e);
-                        }
+            try {
+                final WebScript webScript = AnnotationUtils.findAnnotation(Class.forName(beanClassName), WebScript.class);
+                if (webScript != null) {
+                    String beanName = webScript.value();
+                    if (StringUtils.hasText(beanName)) {
+                        final String webScriptName = "webscript." + beanName + "." + webScript.method().toString().toLowerCase();
+                        factory.removeBeanDefinition(definitionName);
+                        factory.registerBeanDefinition(webScriptName, bd);
+                    } else {
+                        throw new FatalBeanException(String.format("%s is @WebScript annotated, but no value set.", beanClassName));
                     }
-                } catch (NoSuchBeanDefinitionException ex) {
-                    LOGGER.warn(ex.getMessage());
-                    continue;
                 }
+            } catch (ClassNotFoundException e) {
+                logger.error(e.getMessage(), e);
+                throw new FatalBeanException(e.getMessage(), e);
             }
         } else {
-            LOGGER.error("Unable to access to DefaultListableBeanFactory, @WebScript annotation won't work properly");
+            logger.error(String.format("Unable to register '%s' as webscript because beanFactory is not instance of 'DefaultListableBeanFactory'", definitionName));
         }
     }
 }
