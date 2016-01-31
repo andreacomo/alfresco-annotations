b1de7832f65e873ba45acb23cd6094255d8cbe06
andrea.como <andrea.como@gmail.com>
2016-01-31 15:24:30 +0100

    Added all supported format type

--- src/main/java/it/cosenonjaviste/alfresco/annotations/constants/FormatType.java
+++ src/main/java/it/cosenonjaviste/alfresco/annotations/constants/FormatType.java
@@ -3,15 +3,87 @@ package it.cosenonjaviste.alfresco.annotations.constants;
 /**
  * Resources format types.
  *
- * More details on <a href="https://wiki.alfresco.com/wiki/Web_Scripts#Creating_a_Description_Document">Anfresco Wiki</a>
+ * More details on <a href="https://wiki.alfresco.com/wiki/Web_Scripts#Creating_a_Description_Document">WebScript</a>
+ * and <a href="https://wiki.alfresco.com/wiki/3.0_Web_Scripts_Framework#HTTP_Response_Formats">WebScript Framework</a> on Alfresco Wiki
  *
  * @author Andrea Como
  */
 public enum FormatType {
 
+    /**
+     * text/html
+     */
     HTML,
-    JSON,
+
+    /**
+     * text/plain
+     */
+    TEXT,
+
+    /**
+     * text/xml
+     */
     XML,
+
+    /**
+     * application/atom+xml
+     */
+    ATOM,
+
+    /**
+     * application/atom+xml;type=entry
+     */
+    ATOMENTRY,
+
+    /**
+     * application/atom+xml;type=feed
+     */
+    ATOMFEED,
+
+    /**
+     * application/rss+xml
+     */
+    RSS,
+
+    /**
+     * application/json
+     */
+    JSON,
+
+    /**
+     * application/opensearchdescription+xml
+     */
+    OPENSEARCHDESCRIPTION,
+
+    /**
+     * text/plain (Media Wiki markup)
+     */
+    MEDIAWIKI,
+
+    /**
+     * text/html (head & tail chopped)
+     */
+    PORTLET,
+
+    /**
+     * text/html
+     */
+    FBML,
+
+    /**
+     * text/html
+     */
+    PHP,
+
+    /**
+     * text/javascript
+     */
+    JS,
+
+    /**
+     * text/calendar
+     */
+    CALENDAR,
 
     /**
      * In some cases, a URI may decide upon a response content-type at runtime.
